f11df5cf207d85e272d4d25bf2d2e7993ec398b3
Umut Öztürk <umutozmail1@gmail.com>
2025-11-19 14:24:04 +0300

    Initial Commit

---
+++  SwiftUI Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//   SwiftUI Practice
+//
+//  Created by Umut Öztürk on 19.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++  SwiftUI Practice/_SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _SwiftUI_PracticeApp.swift
+//   SwiftUI Practice
+//
+//  Created by Umut Öztürk on 19.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct _SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
